86eb0b4635f48c337423d92c4505eb5d80a3ec89
ajaysingh40cr-maker <ajaysingh40cr@gmail.com>
2026-07-14 14:39:47 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase configuration yahan baad me add karenge.
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
