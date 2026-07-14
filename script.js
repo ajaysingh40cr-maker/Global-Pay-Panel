0bd5e612870b50b5f211fc9badfdefa884f2cc28
ajaysingh40cr-maker <ajaysingh40cr@gmail.com>
2026-07-14 22:14:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ const loginBtn = document.getElementById("showLogin");
 const registerForm = document.getElementById("registerForm");
 const loginForm = document.getElementById("loginForm");
 
-registerBtn.onclick = () => {
+registerBtn.addEventListener("click", () => {
   registerForm.style.display = "block";
   loginForm.style.display = "none";
-};
 
-loginBtn.onclick = () => {
+  registerBtn.classList.add("active");
+  loginBtn.classList.remove("active");
+});
+
+loginBtn.addEventListener("click", () => {
   registerForm.style.display = "none";
   loginForm.style.display = "block";
-};
+
+  loginBtn.classList.add("active");
+  registerBtn.classList.remove("active");
+});
