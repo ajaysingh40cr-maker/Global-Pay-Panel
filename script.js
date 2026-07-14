c73f1fa467ec72e85391812acc05409ab77efe2d
ajaysingh40cr-maker <ajaysingh40cr@gmail.com>
2026-07-14 22:23:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ import {
   GoogleAuthProvider,
   signInWithPopup
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyCL8vjtmJtSIJyax22qaRb3as2UPvmcVEE",
+  authDomain: "global-pay-panel.firebaseapp.com",
+  projectId: "global-pay-panel",
+  storageBucket: "global-pay-panel.firebasestorage.app",
+  messagingSenderId: "50490595463",
+  appId: "1:50490595463:web:69457a3db6634522b3c022",
+  measurementId: "G-ENPBE841ZT"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 const registerBtn = document.getElementById("showRegister");
 const loginBtn = document.getElementById("showLogin");
 
