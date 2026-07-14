4394ed82673134bc408ed128e6129fbba5bf7b20
ajaysingh40cr-maker <ajaysingh40cr@gmail.com>
2026-07-14 15:10:17 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// Firebase configuration yahan baad me add karenge.
-
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCL8vjtmJtSIJvax22qaRb3as2UPvmcVEE",
+  authDomain: "global-pay-panel.firebaseapp.com",
+  projectId: "global-pay-panel",
+  storageBucket: "global-pay-panel.firebasestorage.app",
+  messagingSenderId: "50490595463",
+  appId: "1:50490595463:web:69457a3db6634522b3c022",
+  measurementId: "G-ENPBE84IZT"
 };
